a6185bdf7c22baef4a42c11b0617c740b1f4328b
Franco Carabajal <carabajal.francogabriel@gmail.com>
2025-01-03 15:45:04 -0300

    feat(ergos1): add holdtap module
    Test this first. There's a thing in the linter that doesn't like the
    HT() function

--- ergos1/main.py
+++ ergos1/main.py
@@ -1,9 +1,24 @@
 from kb import KMKKeyboard
 from kmk.keys import KC
 from kmk.modules.split import Split, SplitType
+from kmk.modules.holdtap import HoldTap
 
 keyboard = KMKKeyboard()
 
+holdtap = HoldTap()
+# holdtap.tap_time = 300
+keyboard.modules.append(holdtap)
+# Left side Home row
+HTA = KC.HT(KC.A, KC.LGUI)
+HTS = KC.HT(KC.S, KC.LGUI)
+HTD = KC.HT(KC.D, KC.LGUI)
+HTF = KC.HT(KC.F, KC.LGUI)
+
+# Right side Home row
+HTJ = KC.HT(KC.J, KC.LGUI)
+HTK = KC.HT(KC.K, KC.LGUI)
+HTL = KC.HT(KC.L, KC.LGUI)
+HTSCLN = KC.HT(KC.SCLN, KC.LGUI)
 
 split = Split(
     split_flip = False,  # If both halves are the same, but flipped, set this True
@@ -16,19 +31,20 @@ split = Split(
     #uart_interval=20,  # Sets the uarts delay. Lower numbers draw more power
     #split_target_left=True,  # Assumes that left will be the one on USB. Set to False if it will be the right
 )
+keyboard.modules.append(split)
+
 
 keyboard.keymap = [
     [
-        KC.ESC,  KC.N1, KC.N2,  KC.N3,  KC.N4,   KC.N5,                                         KC.N6,  KC.N7,   KC.N8,   KC.N9,  KC.N0,   KC.QUOT,
-        KC.TAB,  KC.Q,  KC.W,   KC.E,   KC.R,    KC.T,                                          KC.Y,   KC.U,    KC.I,    KC.O,   KC.P,    KC.MINS,
-        KC.LGUI, KC.A,  KC.S,   KC.D,   KC.F,    KC.G,                                          KC.H,   KC.J,    KC.K,    KC.L,   KC.SCLN, KC.EQL,
-        KC.LCTL, KC.Z,  KC.X,   KC.C,   KC.V,    KC.B,   KC.HOME,KC.END,    KC.PSCREEN, KC.CAPS,KC.N,    KC.M,    KC.COMM,KC.DOT,  KC.SLSH, KC.RSHIFT,
+        KC.ESC,  KC.N1, KC.N2,  KC.N3,  KC.N4,   KC.N5,                                          KC.N6,  KC.N7,   KC.N8,   KC.N9,  KC.N0,   KC.QUOT,
+        KC.TAB,  KC.Q,  KC.W,   KC.E,   KC.R,    KC.T,                                           KC.Y,   KC.U,    KC.I,    KC.O,   KC.P,    KC.MINS,
+        KC.LGUI, HTA,   HTS,    HTD,    HTF,     KC.G,                                           KC.H,   HTJ,     HTK,     HTL,    HTSCLN,  KC.EQL,
+        KC.LCTL, KC.Z,  KC.X,   KC.C,   KC.V,    KC.B,   KC.HOME,KC.END,    KC.PSCREEN, KC.CAPS, KC.N,   KC.M,    KC.COMM,KC.DOT,  KC.SLSH, KC.RSHIFT,
                  KC.GRV,KC.BSLS,KC.LEFT,KC.RIGHT,KC.SPC, KC.ENT, KC.PGUP,   KC.TAB,     KC.BSPC, KC.SPC, KC.DOWN, KC.UP,  KC.LBRC, KC.RBRC,
                                                                  KC.PGDOWN, KC.LALT
     ]
 ]
 
-keyboard.modules.append(split)
 print("Keymap loaded with", len(keyboard.keymap[0]), "keys.")
 if __name__ == '__main__':
     keyboard.go()
